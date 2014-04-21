874619cad7a5f0b63ecfb9699deb49239ae2b9d9
hadahash <hadahash@gmail.com>
2014-04-21 20:16:17 +0800

    made changes to files

--- problem1/prob1.c
+++ problem1/prob1.c
@@ -2,7 +2,7 @@
 // Project Euler Problem 1
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
-// gcc -Wall prob1.c -o prob1
+// hadahash@ubuntu:~$ gcc -Wall prob1.c -o prob1
 
 #include <stdio.h>
 
